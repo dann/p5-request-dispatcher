1410e0ae97e50048067acd3be9ee32686ab5223f
Dann <techmemo@gmail.com>
2008-12-08 20:12:55 +0900

    initial commit

---
+++ lib/Request/Dispatcher.pm
@@ -0,0 +1,33 @@
+package Request::Dispatcher;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Request::Dispatcher -
+
+=head1 SYNOPSIS
+
+  use Request::Dispatcher;
+
+=head1 DESCRIPTION
+
+Request::Dispatcher is
+
+=head1 AUTHOR
+
+Takatoshi Kitano E<lt>kitano.tk@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
