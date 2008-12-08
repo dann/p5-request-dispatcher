88c291110cf15b13fd508262e79c9ef623b85b2a
Dann <techmemo@gmail.com>
2008-12-08 21:35:44 +0900

    initial commit

--- lib/Request/Dispatcher.pm
+++ lib/Request/Dispatcher.pm
@@ -1,15 +1,38 @@
 package Request::Dispatcher;
+use Mouse;
+use HTTP::Router;
+use Request::Dispatcher::Dispatch::Simple;
 
-use strict;
-use warnings;
-our $VERSION = '0.01';
+has 'router' => (
+    is      => 'ro',
+    default => sub {
+        HTTP::Router->new;
+    },
+    handles => [qw(add_route)],
+);
+
+no Mouse;
+
+sub dispatch_class {
+    'Request::Dispatcher::Dispatch::Simple';
+}
+
+sub dispatch {
+    my ( $self, $request ) = @_;
+    my $match = $self->router->match( $request->path,
+        { method => $request->method } );
+    my $dispatch = $self->dispatch_class->new( match => $match );
+    return $dispatch;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 __END__
 
 =head1 NAME
 
-Request::Dispatcher -
+Request::Dispatcher - Request dispatcher like Rails
 
 =head1 SYNOPSIS
 
@@ -17,7 +40,7 @@ Request::Dispatcher -
 
 =head1 DESCRIPTION
 
-Request::Dispatcher is
+Request::Dispatcher is the request dispatcher like Rails
 
 =head1 AUTHOR
 
